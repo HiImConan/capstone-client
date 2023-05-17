c72ac0bb4c006090797fda02e7cb492e709d6bd1
HiImConan <mindol0320@naver.com>
2023-05-17 23:07:51 +0900

    edit HTTP header option w/ upgrade insecure request

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    const ContentSecurityPolicy = `
+      upgrade-insecure-requests;
+    `;
+    return [
+      {
+        source: "/search/photo",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
