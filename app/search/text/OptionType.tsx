02567e32af51061eab255329baefe81472baaf41
HiImConan <mindol0320@naver.com>
2023-05-20 00:35:31 +0900

    text search page css done

--- app/search/text/OptionType.tsx
+++ app/search/text/OptionType.tsx
@@ -13,37 +13,62 @@ const OptionType = ({
 }) => {
   const { register } = useFormContext();
   return (
-    <div className="flex justify-between">
-      {OptionType.map((type: IPillTypeInfo) => (
-        <div className="flex">
-          <input
-            type="radio"
-            id={type.name}
-            className="hidden peer"
-            value={type.value}
-            checked={value == type.value}
-            onChange={(e) => onChange(e.target.value)}
-          />
-          <label
-            htmlFor={type.name}
-            key={type.name}
-            className="flex justify-center items-center p-1 cursor-pointer rounded-sm border border-gray-200 peer-checked:border-blue-500 hover:text-gray-600 hover:bg-gray-100"
-          >
-            {type.img.length > 0 ? (
-              <Image
-                src={`/img/${type.img}.jpg`}
-                alt={type.value}
-                width={48}
-                height={48}
-              />
-            ) : (
+    <div className="flex justify-start flex-wrap w-full">
+      {OptionType.map((type: IPillTypeInfo) =>
+        OptionType.length !== 17 ? (
+          <div className="flex">
+            <input
+              type="radio"
+              id={type.name}
+              className="hidden peer"
+              value={type.value}
+              checked={value == type.value}
+              onChange={(e) => onChange(e.target.value)}
+            />
+            <label
+              htmlFor={type.name}
+              key={type.name}
+              className="flex justify-center items-center p-1 cursor-pointer rounded-sm border border-gray-200 peer-checked:border-blue-500 hover:text-gray-600 hover:bg-gray-100"
+            >
+              {type.img.length > 0 ? (
+                <Image
+                  src={`/img/${type.img}.jpg`}
+                  alt={type.value}
+                  width={48}
+                  height={48}
+                />
+              ) : (
+                <span className="w-12 h-12 flex justify-center items-center">
+                  기타
+                </span>
+              )}
+            </label>
+          </div>
+        ) : (
+          <div className="flex">
+            <input
+              type="radio"
+              id={type.name}
+              className="hidden peer"
+              value={type.value}
+              checked={value == type.value}
+              onChange={(e) => onChange(e.target.value)}
+            />
+            <label
+              htmlFor={type.name}
+              key={type.name}
+              className={
+                type.name &&
+                `flex justify-center items-center p-1 cursor-pointer rounded-sm border border-gray-200 peer-checked:border-blue-500 hover:text-${type.name}-600 hover:bg-${type.name}-100`
+              }
+            >
               <span className="w-12 h-12 flex justify-center items-center">
-                기타
+                {type.value}
               </span>
-            )}
-          </label>
-        </div>
-      ))}
+            </label>
+          </div>
+        )
+      )}
     </div>
   );
 };
--- app/search/text/page.tsx
+++ app/search/text/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useForm, FormProvider, Controller } from "react-hook-form";
 import OptionType from "./OptionType";
-import { LineType, PillType, ShapeType } from "./Options";
+import { LineType, PillType, ShapeType, ColorType } from "./Options";
 
 const TextPage = () => {
   const methods = useForm();
@@ -10,47 +10,56 @@ const TextPage = () => {
 
   return (
     <FormProvider {...methods}>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-3/5">
+      <div className="relative shadow-md sm:rounded-lg w-3/5">
         <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full p-5">
           <div className="w-full flex flex-col justify-center items-center">
             <div className="p-5 text-lg font-semibold text-left text-gray-900 bg-white min-w-full">
               알약 직접 검색하기
-              <p className="mt-1 text-sm font-normal text-gray-500">
+              <p className="mt-1 text-sm font-normal text-gray-400">
                 찾고 싶은 알약의 각인, 색상, 모양, 제형 그리고 분할선 정보를
                 입력하세요. 하나의 정보만 입력해도 검색이 되지만, 여러 개를
                 입력할 수록 정확한 결과를 얻을 수 있습니다.
               </p>
             </div>
-            <div>
+            <div className="w-full">
               <div className="flex justify-start items-center gap-1 h-16">
                 <div className="h-full w-24 flex justify-center items-center text-center text-gray-700 uppercase bg-gray-50 font-semibold rounded-t-lg">
                   각인
                 </div>
-                <div className="flex justify-start items-center">
+                <div className="flex justify-start items-center gap-4 w-full">
                   <input
                     {...methods.register("char_front", {
                       min: 1,
                     })}
                     type="text"
                     placeholder="앞면 각인 글자"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-400 focus:border-blue-400 block w-full p-3"
                   />
-                </div>
-                <div className="flex justify-start items-center">
                   <input
                     {...methods.register("char_back", {
                       min: 1,
                     })}
                     type="text"
                     placeholder="뒷면 각인 글자"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-400 focus:border-blue-400 block w-full p-3"
                   />
                 </div>
               </div>
-              <div className="flex justify-start items-center gap-1 h-16">
-                <div className="h-full w-24 flex justify-center items-center text-center text-gray-700 uppercase bg-gray-50 font-semibold ">
+              <div className="flex justify-start items-center gap-1 h-32">
+                <div className="h-32 w-24 flex justify-center items-center text-center text-gray-700 uppercase bg-gray-50 font-semibold">
                   색상
                 </div>
+                <Controller
+                  name="color"
+                  control={methods.control}
+                  render={({ field: { onChange, value } }) => (
+                    <OptionType
+                      OptionType={ColorType}
+                      onChange={onChange}
+                      value={value}
+                    />
+                  )}
+                />
               </div>
               <div className="flex justify-start items-center gap-1 h-16">
                 <div className="h-full w-24 flex justify-center items-center text-center text-gray-700 uppercase bg-gray-50 font-semibold">
