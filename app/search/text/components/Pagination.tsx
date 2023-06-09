b284bd223006af373b13b9c07a11a363c84f3664
HiImConan <mindol0320@naver.com>
2023-06-10 01:08:38 +0900

    add pagination on text search page

---
+++ app/search/text/components/Pagination.tsx
@@ -0,0 +1,106 @@
+const Pagination = ({
+  loadPage,
+  presentPage,
+  lastPage,
+  pageList,
+  resultLength,
+  limit,
+}: {
+  loadPage: (num: number) => void;
+  presentPage: number;
+  lastPage: number;
+  pageList: number[];
+  resultLength: number;
+  limit: number;
+}) => {
+  return (
+    <nav
+      className="flex items-center justify-between pt-4 mb-12"
+      aria-label="Table navigation"
+    >
+      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+        {"전체 "}
+        <span className="font-semibold text-gray-900 dark:text-white">
+          {resultLength}
+        </span>
+        {"개 중 "}
+        <span className="font-semibold text-gray-900 dark:text-white">
+          {presentPage * limit <= resultLength
+            ? `${limit * (presentPage - 1) + 1} - ${presentPage * limit}`
+            : `${limit * (presentPage - 1) + 1} - ${resultLength}`}
+          번째
+        </span>
+      </span>
+      <ul className="inline-flex items-center -space-x-px">
+        <li>
+          <div
+            onClick={() => loadPage(presentPage - 1)}
+            className={
+              presentPage > 1
+                ? "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+                : "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-gray-100 pointer-events-none border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+            }
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </li>
+        {pageList.map((num: number) => (
+          <li>
+            <div
+              key={num}
+              onClick={() => loadPage(num)}
+              className={
+                num == presentPage
+                  ? "px-3 py-2 leading-tight text-gray-500 bg-gray-200 border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                  : "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              }
+            >
+              {num}
+            </div>
+          </li>
+        ))}
+
+        <li>
+          <div
+            onClick={() => loadPage(presentPage + 1)}
+            className={
+              presentPage < lastPage
+                ? "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+                : "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-gray-100 pointer-events-none border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+            }
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- app/search/text/components/ResultCard.tsx
+++ app/search/text/components/ResultCard.tsx
@@ -1,31 +1,144 @@
 import { ISearchResult } from "../types/Options";
+import Image from "next/image";
+import Pagination from "./Pagination";
+import { useState, useEffect } from "react";
 
 const ResultCard = ({ searchResult }: { searchResult: ISearchResult[] }) => {
-  // 빈 배열일 때는 검색결과가 없습니다.
-  // 배열이 차있을 때는 map으로 전개
-  // 약학정보원 긴 카드 형태로 보여주기
+  const [searchResultList, setSearchResultList] = useState<ISearchResult[]>([]);
+  const [pageList, setPageList] = useState([1]);
+  const [presentPage, setPresentPage] = useState(1);
+
+  const limit = 10;
+  const offset = (num: number) => (num - 1) * limit;
+
+  const pageListLength = searchResult.length / limit;
+  const lastPage = Number.isInteger(pageListLength)
+    ? pageListLength
+    : Math.ceil(pageListLength);
+  const wholePageList = [...Array(lastPage).keys()].map(
+    (element) => element + 1
+  );
+
+  const updatePagination = () => {
+    setSearchResultList(
+      searchResult.slice(offset(presentPage), offset(presentPage) + limit)
+    );
+    setPageList(wholePageList.slice(0, 5));
+    setPresentPage(1);
+  };
+
+  useEffect(() => {
+    updatePagination();
+  }, []);
+
+  const loadPage = (num: number) => {
+    setPresentPage(num);
+    setSearchResultList(searchResult.slice(offset(num), offset(num) + limit));
+    console.log(num, lastPage);
+    {
+      lastPage > 5 && num > 3
+        ? num <= lastPage - 3
+          ? setPageList(wholePageList.slice(num - 3, num + 2))
+          : setPageList(wholePageList.slice(lastPage - 5))
+        : setPageList(wholePageList.slice(0, 5));
+    }
+  };
+
   return (
-    <section className="bg-white border border-gray-200 shadow-md rounded-lg flex justify-center items-center">
-      {searchResult.length === 0 ? (
-        <div className="py-8 px-4 mx-auto max-w-screen-xl">
-          <div className="mx-auto max-w-screen-sm text-center">
-            <div className="mb-4 text-3xl tracking-tight font-extrabold text-primary-600">
-              검색 결과가 없습니다.
+    <>
+      <section
+        className={
+          searchResult.length > 0
+            ? "bg-white border border-gray-200 shadow-md rounded-lg flex justify-center items-center"
+            : "bg-white border border-gray-200 shadow-md rounded-lg flex justify-center items-center mb-8"
+        }
+      >
+        {searchResult.length === 0 ? (
+          <div className="py-8 px-4 mx-auto max-w-screen-xl">
+            <div className="mx-auto max-w-screen-sm text-center">
+              <div className="mb-4 text-3xl tracking-tight font-extrabold text-primary-600">
+                검색 결과가 없습니다.
+              </div>
+              <p className="mb-4 text-lg font-light text-gray-500">
+                검색 옵션을 정확하게 설정했는지 확인해보세요.
+              </p>
             </div>
-            <p className="mb-4 text-lg font-light text-gray-500">
-              검색 옵션을 정확하게 설정했는지 확인해보세요.
-            </p>
           </div>
-        </div>
-      ) : (
-        // table로 만들자 그냥
-        <div className="flex flex-col justify-center items-center gap-4">
-          {searchResult.map((pill) => (
-            <div key={pill.name}>{pill.name}</div>
-          ))}
-        </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center text-center first-line:gap-4">
+            <div className="relative overflow-x-auto shadow-md">
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      식별 이미지
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      제품명
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      제형
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      <p>식별표시</p>
+                      <p>{`(앞/뒤)`}</p>
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      색상
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-center">
+                      회사명
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {searchResultList.map((pill) => (
+                    <tr
+                      key={pill.name}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    >
+                      <th
+                        scope="row"
+                        className="px-5 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        <Image
+                          src={pill.pill_img}
+                          width={110}
+                          height={110}
+                          alt="pillimg"
+                        />
+                      </th>
+                      <td className="px-5 py-4 text-center w-3/12">
+                        {pill.name}
+                      </td>
+                      <td className="px-5 py-4 text-center">
+                        {pill.pill_type.replace(/(\|)/g, " / ")}
+                      </td>
+                      <td className="px-5 py-4 text-center">
+                        <p>{pill.char_front}</p>
+                        <p>{pill.char_back}</p>
+                      </td>
+                      <td className="px-5 py-4 text-center">{pill.shape}</td>
+                      <td className="px-5 py-4 text-center">{pill.company}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+      </section>
+      {searchResult.length > limit && (
+        <Pagination
+          loadPage={loadPage}
+          presentPage={presentPage}
+          lastPage={lastPage}
+          pageList={pageList}
+          resultLength={searchResult.length}
+          limit={limit}
+        />
       )}
-    </section>
+    </>
   );
 };
 
